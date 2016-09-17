bc588da6a7f96dd4a7560c15b95d9bce05c06e37
Yuan-Hsin Huang <yuanhsin7211@gmail.com>
2016-09-17 01:44:38 -0700

    Update Ch1_SelectOrAndOrder.sql

--- Ch1_SelectOrAndOrder.sql
+++ Ch1_SelectOrAndOrder.sql
@@ -1,3 +1,224 @@
+--*SQL Ch1--*--
+
+--SELECT/WHERE/AND/OR/ORDER BY/Datatype conversion/
+
+--use [AdventureWORksDW2012]
+
+--SELECT-----------------------------------------------------------------------------------------------
+SELECT 1 
+--A column alias immediately follows a column name in the SELECT clause of a SELECT statement. 
+--Enclose the alias in single or double quotes if it is a reserved keyword or if it contains spaces, punctuation, or special characters. 
+--You can omit the quotes if the alias is a single non-reserved word that contains only letters, digits, or underscores.
+
+--SELECT 1+3 as ID,'9/1/2016' as SDate
+
+SELECT '9/1/2016' AS SDate,'9-1-2013',1 as "Col1",2 as 'Col2'
+
+--
+USE [AdventureWORks2012]
+SELECT *
+FROM [Person].[Person] p
+
+SELECT Count(*)
+FROM [Person].[Person]
+
+SELECT e.BusinessEntityID,e.HireDate,e.JobTitle
+FROM [HumanResources].[Employee] e
+
+SELECT DISTINCT e.ORganizationLevel as 'Organization Level'
+FROM [HumanResources].[Employee] e
+
+SELECT TOP 10 e.BusinessEntityID,e.HireDate,e.JobTitle,e.ORganizationLevel
+FROM [HumanResources].[Employee] e
+
+SELECT TOP 1 PERCENT * 
+FROM Sales.Customer;
+
+SELECT [BusinessEntityID], [SalesYTD]*[CommissionPct] 'Annual Commission'
+FROM [Sales].[SalesPerson]
+
+
+--Using string concatenation
+
+SELECT FirstName,LastName,FirstName+','+LastName AS 'Name'
+FROM [Person].[Person]
+
+ 
+USE AdventureWORks2012;
+GO
+SELECT 'The ORDER due Date: ' + CONVERT(varchar(12), DueDate, 101)
+FROM Sales.SalesORDERHeader
+WHERE SalesORDERID = 50000;
+GO
+
+
+USE AdventureWORks2012;
+GO
+SELECT (LastName + ',' + SPACE(1) + SUBSTRING(FirstName, 1, 1) + '.') AS Name
+FROM Person.Person AS p
+    JOIN HumanResources.Employee AS e
+    ON p.BusinessEntityID = e.BusinessEntityID
+WHERE e.JobTitle LIKE 'Vice%'
+ORDER BY LastName ASC;
+GO
+
+---WHERE------------------------------------------------------------------------------
+
+SELECT *
+FROM [HumanResources].[Employee] 
+WHERE ORganizationLevel = 1
+
+SELECT *
+FROM [HumanResources].[Employee] 
+WHERE JobTitle in( 'Design Engineer','Research AND Development Engineer')
+
+SELECT *
+FROM [HumanResources].[Employee] 
+WHERE 
+--HireDate = '2002-03-03'
+HireDate = '3/3/2002'
+
+SELECT *
+FROM [HumanResources].[Employee] 
+WHERE HireDate between '01/01/2003' AND '01/01/2004'
+
+SELECT 'The list price is ' + CAST(ListPrice AS varchar(12)) AS ListPrice
+FROM Production.Product
+WHERE ListPrice BETWEEN 350.00 AND 400.00;
+
+SELECT *
+FROM [HumanResources].[Employee] 
+WHERE VacationHours>80
+
+SELECT TOP 5 e.BusinessEntityID,e.JobTitle
+FROM [HumanResources].[Employee] e
+WHERE JobTitle like 'Marketing%'
+
+SELECT TOP 5 *
+FROM [HumanResources].[Employee] 
+WHERE [LoginID] like '%works\ken_'
+
+SELECT TOP 5 a.AddressID,a.City
+FROM Person.Address a
+WHERE City = 'Villeneuve-d''Ascq'
+
+--know the difference between '%A%' AND '%A' AND 'A%'?
+SELECT e.FirstName
+FROM [AdventureWORksDW2012].[dbo].[DimEmployee] e
+WHERE e.FirstName like '%A%'
+
+SELECT e.FirstName
+FROM [AdventureWORksDW2012].[dbo].[DimEmployee] e
+WHERE e.FirstName like '%A'
+
+SELECT e.FirstName
+FROM [AdventureWORksDW2012].[dbo].[DimEmployee] e
+WHERE e.FirstName like 'A%'
+
+----AND /OR------------------------------------------------------------------------------------
+
+SELECT *
+FROM [HumanResources].[Employee] 
+WHERE HireDate >'01/01/2003' AND ORganizationLevel=1
+
+SELECT *
+FROM [HumanResources].[Employee] 
+WHERE JobTitle='Design Engineer' OR JobTitle='Chief Financial Officer'
+
+SELECT e.BusinessEntityID,e.HireDate,e.JobTitle
+FROM [HumanResources].[Employee] e
+WHERE HireDate>'01/01/2003'
+      AND (JobTitle='Design Engineer' OR JobTitle='Chief Financial Officer')
+
+SELECT e.BusinessEntityID,e.HireDate,e.JobTitle
+FROM [HumanResources].[Employee] e
+WHERE (HireDate>'01/01/2003' AND JobTitle='Chief Financial Officer') 
+     OR JobTitle='Design Engineer'
+
+
+---ORDER BY -------------------------------------------------------------------------------
+
+SELECT TOP 10 e.BusinessEntityID,e.HireDate,e.JobTitle,e.ORganizationLevel
+FROM [HumanResources].[Employee] e
+ORDER BY e.HireDate
+
+SELECT TOP 10 e.BusinessEntityID,e.HireDate,e.JobTitle,e.ORganizationLevel
+FROM [HumanResources].[Employee] e
+ORDER BY e.HireDate desc
+
+SELECT TOP 10 e.BusinessEntityID,e.HireDate,e.JobTitle,e.ORganizationLevel
+FROM [HumanResources].[Employee] e
+ORDER BY 4,2
+
+
+---Datatype conversion-----------------------------------------------------------------------------------------
+--When convert data types that differ in decimal places,the result value is truncated OR rounded.
+SELECT CAST ( $157.27 AS VARCHAR(10) ),
+       CAST(10.6496 AS int),
+	   CAST(10.6496 AS decimal(6,2)),
+       CAST(10.3496847 AS money),
+	   CONVERT(int, 15.66)
+	  
+SELECT 
+   GETDATE() AS UnconvertedDateTime,
+   CAST(GETDATE() AS nvarchar(30)) AS UsingCast,
+   CONVERT(nvarchar(30), GETDATE(), 126) AS UsingConvert
+
+SELECT CONVERT(nvarchar(30), GETDATE(), 1)    ,1 AS sort
+UNION
+SELECT CONVERT(nvarchar(30), GETDATE(), 101)  ,2 
+UNION
+SELECT CONVERT(nvarchar(30), GETDATE(), 102)  ,3
+UNION
+SELECT CONVERT(nvarchar(30), GETDATE(), 105)  ,4
+UNION
+SELECT CONVERT(nvarchar(30), GETDATE(), 111)  ,5
+
+ORDER BY 2
+
+---conversion Error ----------------------
+SELECT CAST('abc' AS int);
+
+
+
+--sample dataset query for SSRS---------------------------------------------------------------
+--1
+SELECT DISTINCT CountryRegionCode
+  FROM [AdventureWORks2012].[Sales].[SalesTerritORy]
+  ORDER BY 1
+
+--2
+SELECT DISTINCT [DepartmentName]
+FROM [AdventureWORksDW2012].[dbo].[DimEmployee]
+ORDER BY 1
+
+--3
+SELECT DISTINCT    t.[Group] as TerritORy
+FROM Sales.SalesTerritORy t
+ORDER BY 1
+
+--4
+SELECT DISTINCT f.Shift
+FROM            FactCallCenter f
+ORDER BY 1
+
+--5
+SELECT DISTINCT  [EmployeeKey],ParentEmployeeKey
+      ,[SalesTerritORyKey]
+	   ,[DepartmentName]
+      ,[FirstName]
+      ,[LastName]
+      ,[Title]
+      ,[HireDate]
+      ,[PayFrequency]
+      ,[BaseRate]
+      ,[VacationHours]
+	  ,e.Status
+FROM [AdventureWORksDW2012].[dbo].[DimEmployee] e
+WHERE e.ParentEmployeeKey=3
+AND e.Status is not null
+ORDER BY 1,2,4,5
+
 ---SQL Ch1 Project & Solution
 
 --1------------------------------------------------------------------------------------------	
